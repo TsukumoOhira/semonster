c49ad11107a10831e6c92b0d468e81d67d4f2010
TsukumoOhira <m1m23a06@oit.ac.jp>
2023-06-15 13:38:36 +0900

    最初のメッセージの修正

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster!");
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show a new monster!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
-    System.out.println("miss-code");
   }
 }

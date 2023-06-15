85b779a3a7429c39e27699011c4adea8658593aa
TsukumoOhira <m1m23a06@oit.ac.jp>
2023-06-15 12:41:07 +0900

    add semonster main class

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("SEMonster!");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
+    System.out.println("miss-code");
+  }
+}
